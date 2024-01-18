d68759b16dde58f4e2ee347afa87e8aa6bbb9e4f
Kresten Laust <krestenlaust@gmail.com>
2024-01-18 18:19:02 +0100

    Added basic CLI without functionality.

--- src/main/scala/whodarr/classic/cli/main.scala
+++ src/main/scala/whodarr/classic/cli/main.scala
@@ -1,6 +1,19 @@
 package whodarr.classic.cli
 
+import scala.io.StdIn
+
 @main
 def main(): Unit = {
   println("Hello world!")
+
+  println("Enter path to folder> ")
+  val dirpath = StdIn.readLine()
+
+  println("Enter Serial designation> ")
+  val designation = StdIn.readLine()
+
+  println("Enter Serial episode offset> ")
+  val serialEpisodeOffset = StdIn.readInt
+
+
 }
