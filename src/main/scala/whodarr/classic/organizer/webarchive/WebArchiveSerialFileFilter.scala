a44d5192ef3fee9a0d5fc15eef278ae503f6088f
Kresten Laust <krestenlaust@gmail.com>
2024-01-20 16:21:25 +0100

    Fully implemented WebArchive SerialFileFilter

--- src/main/scala/whodarr/classic/organizer/webarchive/WebArchiveSerialFileFilter.scala
+++ src/main/scala/whodarr/classic/organizer/webarchive/WebArchiveSerialFileFilter.scala
@@ -2,6 +2,8 @@ package whodarr.classic.organizer.webarchive
 
 import whodarr.classic.organizer.SerialFileFilter
 
+import java.nio.file.Paths
+
 class WebArchiveSerialFileFilter extends SerialFileFilter:
   override def episodeSubtitleFilePredicate(filePath: String): Boolean =
     filePath.endsWith(".vtt") || filePath.endsWith(".txt")
@@ -10,7 +12,13 @@ class WebArchiveSerialFileFilter extends SerialFileFilter:
     filePath.endsWith(".mp4") || filePath.endsWith(".avi")
 
   override def episodeBonusFilePredicate(filePath: String): Boolean =
-    ???
+    getEpisodeInfoPart(filePath).endsWith("Bonus")
 
   override def episodeFileInSerialPredicate(filePath: String, serialNumber: Int): Boolean =
-    ???
+    getEpisodeInfoPart(filePath).contains("(%03d)".format(serialNumber))
+
+  private def getFilename(filePath: String): String =
+    Paths.get(filePath).getFileName.toString
+
+  private def getEpisodeInfoPart(filePath: String): String =
+    getFilename(filePath).split(" - ")(1)
