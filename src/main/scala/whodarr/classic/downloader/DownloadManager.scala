1c5bf2a199a0a707a4352370d52ef2c40c1660c0
Kresten Laust <krestenlaust@gmail.com>
2024-01-24 23:36:31 +0100

    Drafted download manager

---
+++ src/main/scala/whodarr/classic/downloader/DownloadManager.scala
@@ -0,0 +1,14 @@
+package whodarr.classic.downloader
+
+import whodarr.classic.types.{EpisodeId, EpisodeMedia}
+
+import scala.collection.mutable
+import scala.concurrent.Future
+
+class DownloadManager:
+  val queuedEpisodes: mutable.HashSet[EpisodeId] = mutable.HashSet()
+  val acquirers: List[MediaAcquirer] = List()
+
+  def downloadEpisode(episodeId: EpisodeId): List[Future[Option[EpisodeMedia]]] =
+    queuedEpisodes.add(episodeId)
+    acquirers.map(a => a.acquireMedia(episodeId))
