bd8800a8af7be2f46e6ec10361c895b7baebf679
Kresten Laust <krestenlaust@gmail.com>
2024-01-27 11:51:03 +0100

    Refactored move

--- src/main/scala/whodarr/classic/util/FileUtility.scala
+++ src/main/scala/whodarr/classic/util/FileUtility.scala
@@ -1,12 +1,15 @@
 package whodarr.classic.util
 
-import java.nio.file.{Paths, Files}
+import java.io.File
+import java.nio.file.{Files, Paths}
 import scala.collection.JavaConverters.*
 import scala.util.Try
 
 object FileUtility:
-  def move(srcPath: String, dstPath: String): Try[Unit] =
-    Try(Files.move(Paths.get(srcPath), Paths.get(dstPath)))
+  def move(srcPath: String, dstPath: String): Boolean =
+    val fileToMove = File(srcPath)
+    val targetFile = File(dstPath)
+    fileToMove.renameTo(targetFile)
 
   def getFilePathsInFolder(folderPath: String): Try[Seq[String]] =
     Try(Files.list(Paths.get(folderPath)).iterator().asScala.toSeq.map(p => p.toString))
