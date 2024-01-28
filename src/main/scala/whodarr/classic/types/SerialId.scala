378332d3e0dce35dfa8f8ad30b85244a1fa82637
Kresten Laust <krestenlaust@gmail.com>
2024-01-28 11:48:16 +0100

    Simplified SerialId to only include what makes it unique

--- src/main/scala/whodarr/classic/types/SerialId.scala
+++ src/main/scala/whodarr/classic/types/SerialId.scala
@@ -1,10 +1,7 @@
 package whodarr.classic.types
 
 /**
- *
- * @param season The season which the serial is placed in.
- * @param relativeEpisodeOffset The offset of the serial in the season, in episode-units.
- * @param absoluteStoryNumber The inter-seasonal story number of the serial.
- * @param relativeStoryNumber The story number within the season.
+ * Used to ID serials.
+ * @param storyNumber The inter-seasonal story number of the serial.
  */
-case class SerialId(season: Int, relativeEpisodeOffset: Int, absoluteStoryNumber: Int, relativeStoryNumber: Int)
+case class SerialId(storyNumber: Int)
