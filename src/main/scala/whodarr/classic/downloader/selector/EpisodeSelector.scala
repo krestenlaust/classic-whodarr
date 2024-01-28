eeda79787cde70a6e3713e909cf77a3c6fdaf806
Kresten Laust <krestenlaust@gmail.com>
2024-01-28 14:06:43 +0100

    Refactored episode selector

--- src/main/scala/whodarr/classic/downloader/selector/EpisodeSelector.scala
+++ src/main/scala/whodarr/classic/downloader/selector/EpisodeSelector.scala
@@ -3,4 +3,4 @@ package whodarr.classic.downloader.selector
 import whodarr.classic.types.EpisodeId
 
 trait EpisodeSelector:
-  def episodes: Seq[EpisodeId]
+  def selectEpisodes(episodes: Seq[EpisodeId]): Seq[EpisodeId]
--- src/main/scala/whodarr/classic/downloader/selector/LookAheadEpisodeSelector.scala
+++ src/main/scala/whodarr/classic/downloader/selector/LookAheadEpisodeSelector.scala
@@ -2,4 +2,4 @@ package whodarr.classic.downloader.selector
 import whodarr.classic.types.EpisodeId
 
 class LookAheadEpisodeSelector extends EpisodeSelector:
-  override def episodes: Seq[EpisodeId] = ???
+  override def selectEpisodes(episodes: Seq[EpisodeId]): Seq[EpisodeId] = ???
