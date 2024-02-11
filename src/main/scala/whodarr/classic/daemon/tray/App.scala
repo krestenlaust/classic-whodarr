19b004867bfff941428d9b1414363806f0119da5
Kresten Laust <krestenlaust@gmail.com>
2024-02-11 12:59:21 +0100

    Add 'Github page' menuitem to tray-app

--- src/main/scala/whodarr/classic/daemon/tray/App.scala
+++ src/main/scala/whodarr/classic/daemon/tray/App.scala
@@ -1,7 +1,10 @@
 package whodarr.classic.daemon.tray
 
+import dorkbox.desktop.Desktop
 import dorkbox.systemTray
 import dorkbox.systemTray.{Menu, MenuItem, Separator, SystemTray}
+
+import scala.util.Try
 
 class App:
   def run =
@@ -12,6 +15,9 @@ class App:
 
   def buildMenu(menu: Menu, systemTray: SystemTray) =
     menu.add(new Separator)
+
+    val menuItemAbout = new MenuItem("Github Page", _ => Try(Desktop.browseURL("https://github.com/krestenlaust/classic-whodarr")))
+    menu.add(menuItemAbout)
 
     val menuItemExit = new MenuItem("Exit Service", _ => systemTray.shutdown())
     menu.add(menuItemExit)
