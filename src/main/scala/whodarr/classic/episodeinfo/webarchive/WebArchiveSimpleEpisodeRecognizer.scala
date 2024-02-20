3817052082f25fb4f1cd51768d54f5adcdc1fc21
Kresten Laust <krestenlaust@gmail.com>
2024-02-20 23:12:55 +0100

    Resolve issue with paths in episode recognizer

--- src/main/scala/whodarr/classic/episodeinfo/webarchive/WebArchiveSimpleEpisodeRecognizer.scala
+++ src/main/scala/whodarr/classic/episodeinfo/webarchive/WebArchiveSimpleEpisodeRecognizer.scala
@@ -2,13 +2,16 @@ package whodarr.classic.episodeinfo.webarchive
 
 import whodarr.classic.episodeinfo.{EpisodeId, EpisodeMedia, EpisodeRecognizer}
 
+import java.nio.file.Paths
+
 /**
  * A simple episode recognizer. Depends on getting episode offset in the season.
  * @param episodeOffset The offset (0-indexed) of the first episode in the serial.
  */
 class WebArchiveSimpleEpisodeRecognizer(episodeOffset: Int) extends EpisodeRecognizer:
   override def detectFromPath(filePath: String): Option[EpisodeId] =
-    filePath.split(" - ") match {
+    Paths.get(filePath).getFileName.toString
+      .split(" - ") match {
       case Array(_, serialDesignation, episodeName) =>
         for {
           season <- serialDesignation.split(" ")(0).substring(1, 3).toIntOption
