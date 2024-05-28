7bfbb1cbc508728f7bc00743f057e9c5ec30fd36
Kresten Laust <krestenlaust@gmail.com>
2024-05-28 21:07:21 +0200

    Replace all use of NIO with os-lib

--- src/main/scala/whodarr/classic/episodeinfo/webarchive/WebArchiveSerialFileFilter.scala
+++ src/main/scala/whodarr/classic/episodeinfo/webarchive/WebArchiveSerialFileFilter.scala
@@ -1,27 +1,30 @@
 package whodarr.classic.episodeinfo.webarchive
 
 import whodarr.classic.episodeinfo.SerialFileFilter
-
-import java.nio.file.Paths
+import os.Path
 
 class WebArchiveSerialFileFilter extends SerialFileFilter:
-  override def episodeSubtitleFilePredicate(filePath: String): Boolean =
-    filePath.endsWith(".vtt") || filePath.endsWith(".txt")
+  override def episodeSubtitleFilePredicate(filePath: Path): Boolean =
+    getFileExtension(filePath).exists(ext => ext.equalsIgnoreCase("vtt") || ext.equalsIgnoreCase("txt"))
 
-  override def episodeVideoFilePredicate(filePath: String): Boolean =
-    filePath.endsWith(".mp4") || filePath.endsWith(".avi")
+  override def episodeVideoFilePredicate(filePath: Path): Boolean =
+    getFileExtension(filePath).exists(ext => ext.equalsIgnoreCase("mp4") || ext.equalsIgnoreCase("avi"))
 
-  override def episodeBonusFilePredicate(filePath: String): Boolean =
+  override def episodeBonusFilePredicate(filePath: Path): Boolean =
     getEpisodeInfoPart(filePath).endsWith("Bonus")
 
   override def episodeFileInSerialPredicate(
-      filePath: String,
+      filePath: Path,
       serialNumber: Int
   ): Boolean =
     getEpisodeInfoPart(filePath).contains("(%03d)".format(serialNumber))
 
-  private def getEpisodeInfoPart(filePath: String): String =
+  private def getEpisodeInfoPart(filePath: Path): String =
     getFilename(filePath).split(" - ")(1)
 
-  private def getFilename(filePath: String): String =
-    Paths.get(filePath).getFileName.toString
+  private def getFilename(filePath: Path): String =
+    filePath.last
+
+  private def getFileExtension(filePath: Path): Option[String] =
+    filePath.lastOpt
+      .flatMap(p => p.split('.').lastOption)
--- src/main/scala/whodarr/classic/util/FileUtility.scala
+++ src/main/scala/whodarr/classic/util/FileUtility.scala
@@ -1,16 +1,14 @@
 package whodarr.classic.util
 
 import java.io.File
-import java.nio.file.{ Files, Path, Paths }
-import scala.collection.JavaConverters.*
 import scala.util.Try
-import os._
+import os.Path
 
 object FileUtility:
-  def move(srcPath: os.Path, dstPath: os.Path): Boolean =
+  def move(srcPath: Path, dstPath: Path): Boolean =
     val fileToMove = File(srcPath.toString)
     val targetFile = File(dstPath.toString)
     fileToMove.renameTo(targetFile)
 
-  def getFilePathsInFolder(folderPath: os.Path): Try[IndexedSeq[os.Path]] =
+  def getFilePathsInFolder(folderPath: Path): Try[IndexedSeq[Path]] =
     Try(os.list(folderPath))
