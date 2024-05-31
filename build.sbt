9de921c286bc3a570fa07153f8cc424171b4cd0d
Kresten Laust <krestenlaust@gmail.com>
2024-05-31 15:18:40 +0200

    Add Doctor Who Season 10 fixture (#30)
    * Add Doctor Who Season 10 fixture
    
    * Reorganize into resources folder
    
    * Add empty integration test
    
    * Update test boilerplate
    
    * Add correct fixture to compare to
    
    * Unpack fixtures and cleanup after test
    
    * Comment out test template

--- build.sbt
+++ build.sbt
@@ -8,7 +8,8 @@ lazy val root = (project in file("."))
   )
 
 libraryDependencies ++= Seq(
-  "org.scalatest" %% "scalatest" % "3.2.17" % "test",
-  "com.lihaoyi"   %% "os-lib"    % "0.10.1",
-  "com.typesafe"   % "config"    % "1.4.3"
+  "org.scalatest"    %% "scalatest" % "3.2.17" % "test",
+  "com.lihaoyi"      %% "os-lib"    % "0.10.1",
+  "com.typesafe"      % "config"    % "1.4.3",
+  "net.lingala.zip4j" % "zip4j"     % "2.11.5"
 )
---
+++ src/test/scala/whodarr/classic/organizer/webarchive/tests/WebArchiveOrganizerIntegrationTests.scala
@@ -0,0 +1,37 @@
+package whodarr.classic.organizer.webarchive.tests
+
+import net.lingala.zip4j.ZipFile
+import org.scalatest.Outcome
+import org.scalatest.flatspec.FixtureAnyFlatSpec
+import os.Path
+
+import java.nio.file.Paths
+
+class WebArchiveOrganizerIntegrationTests extends FixtureAnyFlatSpec {
+  val resourceRoot: Path = os.Path(java.nio.file.Paths.get(getClass.getResource("/").toURI))
+  val fixtureRoot: Path  = resourceRoot / "fixtures"
+  val fixtureName        = "doctorwho-s10"
+  val fixtureCorrectName = "doctorwho-s10_correct"
+
+  override protected def withFixture(test: OneArgTest): Outcome =
+    val fixtureSource        = fixtureRoot / f"${fixtureName}.zip"
+    val fixtureCorrectSource = fixtureRoot / f"${fixtureCorrectName}.zip"
+
+    val fixturePath        = resourceRoot / "tempFixture" / fixtureName
+    val fixtureCorrectPath = resourceRoot / "tempCorrectFixture" / fixtureCorrectName
+
+    // Unpack fixtures
+    new ZipFile(fixtureSource.toString).extractAll(fixturePath.toString)
+    new ZipFile(fixtureCorrectSource.toString).extractAll(fixtureCorrectPath.toString)
+
+    try test((fixturePath, fixtureCorrectPath))
+    finally
+      os.remove.all(fixturePath)
+      os.remove.all(fixtureCorrectPath)
+
+  override protected type FixtureParam = (os.Path, os.Path)
+
+  /*it should "have moved serial no. 66" in { case (rootFolder, organizedRootFolder) =>
+    ???
+  }*/
+}
